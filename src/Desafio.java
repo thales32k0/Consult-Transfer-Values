9d555a57bb3133dbabb2548b857702cd9703e0bf
Thales Eduardo Pedro <thales_edu_@hotmail.com>
2023-09-03 19:36:04 -0300

    Initial commit

---
+++ src/Desafio.java
@@ -0,0 +1,14 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Thales Eduardo";
+        String tipoConta = "Corrente";
+        double saldo = 2000.00;
+
+        System.out.println("***************");
+        System.out.println("\nNome do cliente:" + nome);
+        System.out.println("Tipo conta " + tipoConta);
+        System.out.println("Saldo atual " + saldo);
+        System.out.println("\n***************");
+
+    }
+}
